a7d85da9280c9eb192d0290ae6fbb5701fca79b1
FelixjYD <lukaking9878@gmail.com>
2024-05-27 17:55:31 -0600

    Des-protección rutas

--- routes/api.php
+++ routes/api.php
@@ -12,34 +12,27 @@ use App\Http\Controllers\UserController;
 
 Route::get('/users', [UserController::class, 'index']);
 
-
 // Rutas de autenticación (no requieren autenticación)
 Route::post('/login', [AuthenticatedSessionController::class, 'store']);
 Route::post('/register', [RegisteredUserController::class, 'store']);
 Route::post('/forgot-password', [PasswordResetLinkController::class, 'store']);
 Route::post('/reset-password', [NewPasswordController::class, 'store']);
 
-
-
-
-// Rutas protegidas por autenticación
-Route::middleware(['auth:sanctum'])->group(function () {
-    Route::get('/user', function (Request $request) {
-        return $request->user();
-    });
-
-    Route::post('/logout', [AuthenticatedSessionController::class, 'destroy']);
-
-
-    Route::get('/expenses', [ExpenseController::class, 'index']);
-    Route::post('/expenses', [ExpenseController::class, 'store']);
-    Route::get('/expenses/{expense}', [ExpenseController::class, 'show']);
-    Route::put('/expenses/{expense}', [ExpenseController::class, 'update']);
-    Route::delete('/expenses/{expense}', [ExpenseController::class, 'destroy']);
-
-    Route::get('/incomes', [IncomeController::class, 'index']);
-    Route::post('/incomes', [IncomeController::class, 'store']);
-    Route::get('/incomes/{income}', [IncomeController::class, 'show']);
-    Route::put('/incomes/{income}', [IncomeController::class, 'update']);
-    Route::delete('/incomes/{income}', [IncomeController::class, 'destroy']);
+// Rutas que anteriormente estaban protegidas por autenticación
+Route::get('/user', function (Request $request) {
+    return $request->user();
 });
+
+Route::post('/logout', [AuthenticatedSessionController::class, 'destroy']);
+
+Route::get('/expenses', [ExpenseController::class, 'index']);
+Route::post('/expenses', [ExpenseController::class, 'store']);
+Route::get('/expenses/{expense}', [ExpenseController::class, 'show']);
+Route::put('/expenses/{expense}', [ExpenseController::class, 'update']);
+Route::delete('/expenses/{expense}', [ExpenseController::class, 'destroy']);
+
+Route::get('/incomes', [IncomeController::class, 'index']);
+Route::post('/incomes', [IncomeController::class, 'store']);
+Route::get('/incomes/{income}', [IncomeController::class, 'show']);
+Route::put('/incomes/{income}', [IncomeController::class, 'update']);
+Route::delete('/incomes/{income}', [IncomeController::class, 'destroy']);
